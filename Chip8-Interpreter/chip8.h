518dff92e170e2dca162cd69e1fee0c53f5a54b0
Bailey Evanoff <bailey.evanoff@gmail.com>
2021-10-23 20:16:21 -0700

    Add project files.

---
+++ Chip8-Interpreter/chip8.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class chip8 {
+
+	private:
+		
+		unsigned short opcode;
+		unsigned char memory[4096];
+
+		unsigned char V[16];
+
+		unsigned short I;
+
+		unsigned short pc;
+
+		unsigned char gfx[64 * 32];
+
+		unsigned char delay_timer;
+		unsigned char sound_timer;
+
+		unsigned short stack[16];
+		unsigned short sp;
+
+		unsigned char key[16];
+
+};
